e1c12b7d3a4a66162840e139d2e45eb6c7445655
tnchunit <tnchunit@gmail.com>
2019-08-14 15:13:21 +0800

    後台加入所見即所得編輯器

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -71,7 +71,7 @@ class ProductController extends AdminController
         $form = new Form(new Product);
 
         $form->text('title', __('Title'))->rules('required');
-        $form->textarea('description', __('Description'))->rules('required');
+        $form->ckeditor('description', __('Description'))->rules('required');
         $form->image('image', __('Image'))->rules('required');
         $states = [
             'on'  => ['value' => 1, 'text' => '是', 'color' => 'success'],
